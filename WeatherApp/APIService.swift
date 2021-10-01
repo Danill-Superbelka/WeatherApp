08860d8cb25ead1a0d844c4f64b42f094b5c18aa
Daniil <51852490+Danill-Superbelka@users.noreply.github.com>
2021-10-01 15:18:36 +0300

    Creating model WeatherForecast

---
+++ WeatherApp/APIService.swift
@@ -0,0 +1,10 @@
+//
+//  APIService.swift
+//  WeatherApp
+//
+//  Created by Даниил  on 01.10.2021.
+//
+
+import Foundation
+
+
---
+++ WeatherApp/Weather.swift
@@ -0,0 +1,51 @@
+//
+//  Weather.swift
+//  WeatherApp
+//
+//  Created by Даниил  on 01.10.2021.
+//
+
+import Foundation
+
+struct Weather: Codable {
+    let id: Int
+    let icon: String
+    var weatherIconUrl: URL {
+    let imageURL = "http://openweathermap.org/img/wn/\(icon)@2x.png"
+        return URL(string: imageURL)!
+        }
+}
+
+struct WeatherForecast: Codable {
+    
+    struct Hourly: Codable {
+        
+        let dt: Date
+        let temp: Double
+        let feels_like:Double
+        let humidity: Int
+        let clouds: Int
+        let wind_speed: Double
+        let weather: [Weather]
+    }
+    
+    struct Daily: Codable {
+        
+        struct Temp: Codable {
+            let max: Double
+            let min: Double
+        }
+        struct FeelsLike: Codable {
+            let day: Double
+        }
+        
+        let dt: Date
+        let temp: Temp
+        let feels_like: FeelsLike
+        let humidity: Int
+        let weather: [Weather]
+    }
+    
+    let hourly: Hourly
+    let daily: [Daily]
+}
