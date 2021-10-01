eab04515109827bc1f38ce49248d3361977331fc
Daniil <51852490+Danill-Superbelka@users.noreply.github.com>
2021-10-01 22:41:57 +0300

    Create APIService

--- WeatherApp/APIService.swift
+++ WeatherApp/APIService.swift
@@ -7,4 +7,28 @@
 
 import Foundation
 
-
+public class APIService {
+    
+    public let shared = APIService()
+    
+    public func getJSON (stringURL: String) {
+        guard let url = URL(string: stringURL) else {
+            print("Ошибка получения URL адреса")
+            return
+        }
+        
+        let request = URLRequest(url: url)
+        URLSession.shared.dataTask(with: request) {data, response, error in
+            guard let data = data else {
+                print("Ошибка в получение данных")
+                return
+            }
+            
+            let decoder = JSONDecoder()
+            guard let forecast = try? decoder.decode(WeatherForecast.self, from: data) else {
+                print("Ошибка декодирования данных")
+                return
+            }
+        }.resume()
+    }
+}
